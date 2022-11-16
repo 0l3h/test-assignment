b4d37e2e7264109c531aedf781033027ea9c753a
0l3h <kubik7950@gmail.com>
2022-11-16 22:39:59 +0200

    Add test data to list

--- client/js/main.js
+++ client/js/main.js
@@ -1,36 +1,59 @@
 'use strict'
 
-const usernames = ['username_1', 'username_2', 'username_3', 'username_4'];
+const users = [
+    {
+        order: (Math.random() * 100).toFixed(0),
+        username: 'First'
+    },
+    {
+        order: (Math.random() * 100).toFixed(0),
+        username: 'Second'
+    },
+    {
+        order: (Math.random() * 100).toFixed(0),
+        username: 'Third'
+    },
+    {
+        order: (Math.random() * 100).toFixed(0),
+        username: 'Fourth'
+    },
+    {
+        order: (Math.random() * 100).toFixed(0),
+        username: 'Fifth'
+    }
+];
 
 getUsernames();
-
-const form = document.querySelector('form');
-
-form.addEventListener('submit', addUsername);
-form.addEventListener('submit', getUsernames);
 
 function addUsername(e) {
     e.preventDefault();
 
     const usernameInput = form.elements[0];
-    const inputValue = usernameInput.value;
+    const username = usernameInput.value;
     
-    usernames.push(inputValue);
+    users.push({order: (Math.random() * 100).toFixed(0), username});
 };
 
 function getUsernames() {
     const orderedList = document.querySelector('ol');
     orderedList.replaceChildren();
 
-    for (const name of usernames) {
+    for (const user of users) {
         const listElement = document.createElement('li');
+        const order = document.createElement('span');
         const deleteButton = document.createElement('button');
         const editButton = document.createElement('button');
+        const usernameElement = document.createElement('span');
 
+        order.textContent = user.order;
+        usernameElement.textContent = user.username;
         editButton.textContent = 'Edit';
         deleteButton.textContent = 'Delete';
+
+        deleteButton.addEventListener('click', deleteUsername);
         
-        listElement.textContent = name;
+        listElement.appendChild(order);
+        listElement.appendChild(usernameElement);
         listElement.appendChild(editButton);
         listElement.appendChild(deleteButton);
 
@@ -43,5 +66,10 @@ function updateUsername() {
 };
 
 function deleteUsername() {
-
+    
 };
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', addUsername);
+form.addEventListener('submit', getUsernames);
