ba49b6d4845ca3577a8417ccf939c7524b7a0547
0l3h <kubik7950@gmail.com>
2022-11-16 17:02:39 +0200

    Added create functionality

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,41 @@
 'use strict'
 
-function addUsername() {
-    form.submit();
+const usernames = ['username_1', 'username_2', 'username_3', 'username_4'];
+
+getUsernames();
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', addUsername);
+form.addEventListener('submit', getUsernames);
+
+function addUsername(e) {
+    e.preventDefault();
+
+    const usernameInput = form.elements[0];
+    const inputValue = usernameInput.value;
+    
+    usernames.push(inputValue);
 };
 
-function getUsername() {
+function getUsernames() {
+    const orderedList = document.querySelector('ol');
+    orderedList.replaceChildren();
 
+    for (const name of usernames) {
+        const listElement = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        const editButton = document.createElement('button');
+
+        editButton.textContent = 'Edit';
+        deleteButton.textContent = 'Delete';
+        
+        listElement.textContent = name;
+        listElement.appendChild(editButton);
+        listElement.appendChild(deleteButton);
+
+        orderedList.appendChild(listElement);
+    };
 };
 
 function updateUsername() {
