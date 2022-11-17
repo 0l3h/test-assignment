a7fccbd610865000023315682f99c662e6921e98
0l3h <kubik7950@gmail.com>
2022-11-17 20:40:15 +0200

    Added API endpoints to app.js file

--- server/app.js
+++ server/app.js
@@ -6,7 +6,12 @@ const port = process.env.PORT || 5000;
 
 app.disable('x-powered-by');
 
-app.use('/', express.static(path.resolve(__dirname + '/../client/')));
+app.use(express.static(path.resolve(__dirname + '/../client/')));
+
+app.get('/', () => {});
+app.post('/', () => {});
+app.patch('/', () => {});
+app.delete('/', () => {});
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
