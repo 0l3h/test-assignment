216229933c41d0372816d8673e2edb817d9b4e84
0l3h <kubik7950@gmail.com>
2022-11-18 21:00:45 +0200

    Added controllers to app.js file

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { getAllUsers, createUser, updateUser, deleteUser } = require('./controllers/user.controller');
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -7,11 +8,12 @@ const port = process.env.PORT || 5000;
 app.disable('x-powered-by');
 
 app.use(express.static(path.resolve(__dirname + '/../client/')));
+app.use(express.json());
 
-app.get('/', () => {});
-app.post('/', () => {});
-app.patch('/', () => {});
-app.delete('/', () => {});
+app.get('/get-users', getAllUsers);
+app.post('/create-user', createUser);
+app.patch('/update-user/:id', updateUser);
+app.delete('/delete-user/:id', deleteUser);
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
