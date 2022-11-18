d5488313002f8a8d7e4c8f9b4d8a7badf2cc06ec
0l3h <kubik7950@gmail.com>
2022-11-18 21:02:21 +0200

    Implemented getting, deleting and adding a user

--- client/js/main.js
+++ client/js/main.js
@@ -1,42 +1,24 @@
 'use strict'
 
-const users = [
-    {
-        order: (Math.random() * 100).toFixed(0),
-        username: 'First'
-    },
-    {
-        order: (Math.random() * 100).toFixed(0),
-        username: 'Second'
-    },
-    {
-        order: (Math.random() * 100).toFixed(0),
-        username: 'Third'
-    },
-    {
-        order: (Math.random() * 100).toFixed(0),
-        username: 'Fourth'
-    },
-    {
-        order: (Math.random() * 100).toFixed(0),
-        username: 'Fifth'
-    }
-];
+import { createUserRequest, getAllUsersRequest, updateUserRequest, deleteUserRequest } from '../api/user.js';
 
-getUsernames();
+getUsers();
 
-function addUsername(e) {
+async function addUser(e) {
     e.preventDefault();
 
     const usernameInput = form.elements[0];
     const username = usernameInput.value;
-    
-    users.push({order: (Math.random() * 100).toFixed(0), username});
+    const user = {order: (Math.random() * 1000).toFixed(0), username};
+
+    createUserRequest(user);
 };
 
-function getUsernames() {
+async function getUsers() {
     const orderedList = document.querySelector('ol');
     orderedList.replaceChildren();
+
+    const users = await (await getAllUsersRequest()).json();
 
     for (const user of users) {
         const listElement = document.createElement('li');
@@ -46,11 +28,14 @@ function getUsernames() {
         const usernameElement = document.createElement('span');
 
         order.textContent = user.order;
-        usernameElement.textContent = user.username;
+        usernameElement.textContent = user.name;
         editButton.textContent = 'Edit';
         deleteButton.textContent = 'Delete';
 
-        deleteButton.addEventListener('click', deleteUsername);
+        deleteButton.addEventListener('click', deleteUser);
+        editButton.addEventListener('click', updateUser);
+
+        listElement.setAttribute('data-user-id', user.id);
         
         listElement.appendChild(order);
         listElement.appendChild(usernameElement);
@@ -61,15 +46,24 @@ function getUsernames() {
     };
 };
 
-function updateUsername() {
+async function updateUser(e) {
+    const listElement = e.target.parentElement;
+    const userId = listElement.getAttribute('data-user-id');
+    const inputElement = document.querySelector(`li[data-user-id=${userId}]`);
 
+    console.log(inputElement.value);
+
+    updateUserRequest(userId);
 };
 
-function deleteUsername() {
-    
+async function deleteUser(e) {
+    const listElement = e.target.parentElement;
+    const userId = listElement.getAttribute('data-user-id');
+
+    deleteUserRequest(userId).then(getUsers);
 };
 
 const form = document.querySelector('form');
 
-form.addEventListener('submit', addUsername);
-form.addEventListener('submit', getUsernames);
+form.addEventListener('submit', addUser);
+form.addEventListener('submit', getUsers);
