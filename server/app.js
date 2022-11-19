0ac8e35ab5e761a3b7ac1149f8ef2368eb08b6ad
0l3h <kubik7950@gmail.com>
2022-11-19 21:45:05 +0200

    Added change rank controller to server endpoint

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const path = require('path');
-const { getAllUsers, createUser, updateUser, deleteUser } = require('./controllers/user.controller');
+const { 
+    getAllUsers, 
+    createUser,
+    updateUsername, 
+    changeRank, 
+    deleteUser 
+} = require('./controllers/user.controller');
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -12,7 +19,8 @@ app.use(express.json());
 
 app.get('/get-users', getAllUsers);
 app.post('/create-user', createUser);
-app.patch('/update-user/:id', updateUser);
+app.patch('/update-username/:id', updateUsername);
+app.patch('/change-rank/:id', changeRank);
 app.delete('/delete-user/:id', deleteUser);
 
 app.listen(port, () => {
